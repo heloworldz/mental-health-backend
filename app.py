ac39f9ad5b35bd1f74f41658b860fac89c70216b
Undead <daveaye11@gmail.com>
2025-03-22 11:26:22 +0530

    Added home route

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def chat():
     return jsonify({"response": response, "sentiment": sentiment})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
